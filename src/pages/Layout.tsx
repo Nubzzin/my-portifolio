d01e91c325733b7b538f8f6e7a3b83e34fcfcfa9
Nubzzin <matheus.thelas@hotmail.com>
2025-07-25 18:30:02 -0300

    Add translations

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import LanguageSwitcher from "../components/LanguageSwitcher";
 
-const Layout = () => (
-  <div>
-    <Outlet />
-    <LanguageSwitcher />
-  </div>
-);
+function Layout() {
+  return (
+    <div>
+      <Outlet />
+      <LanguageSwitcher />
+    </div>
+  );
+}
 
 export default Layout;
